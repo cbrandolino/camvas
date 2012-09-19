27c208d84709e030b892ca6e9efe64a942faa92d
Claudio Brandolino <cbrandolino@gmail.com>
2012-09-19 06:52:58 +0200

    Video now shown in canvas element; Camorama object introduced
    The original video element is now hidden.

--- js/base.js
+++ js/base.js
@@ -5,15 +5,40 @@ navigator.getUserMedia  = navigator.getUserMedia ||
                           navigator.mozGetUserMedia || 
                           navigator.msGetUserMedia
 
-window.onload = function() {
-  var streamContainer = document.createElement('div')
-  var video = document.createElement('video')
-  video.setAttribute('autoplay', '1')
-  streamContainer.appendChild(video)
-  document.body.appendChild(streamContainer)
+window.requestAnimationFrame = window.requestAnimationFrame ||
+                               window.webkitRequestAnimationFrame ||
+                               window.mozRequestAnimationFrame ||
+                               window.msRequestAnimationFrame ||
+                               window.oRequestAnimationFrame
 
+window.onload = function(){new camorama()}
+
+function camorama() {
+  var self = this
+  this.ctx = document.getElementsByTagName('canvas')[0].getContext('2d')
+  var streamContainer = document.createElement('div')
+  this.video = document.createElement('video')
+  this.video.setAttribute('autoplay', '1')
+  this.video.setAttribute('style', 'display:none')
+  streamContainer.appendChild(this.video)
+  document.body.appendChild(streamContainer)
   navigator.getUserMedia({video: true}, function(stream) {
-    video.src = window.URL.createObjectURL(stream)
+    self.video.src = window.URL.createObjectURL(stream)
     localMediaStream = stream
+    self.update()
   })
+
+  this.update = function() {
+    var self = this
+    var loop = function() {
+      self.draw()
+      requestAnimationFrame(loop) 
+    }
+    requestAnimationFrame(loop) 
+  } 
+
+  this.draw = function() {
+    this.ctx.drawImage(this.video, 0, 0)
+  }
 }
+
