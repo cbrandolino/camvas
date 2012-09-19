728fcca88c3af84886a18e89d56a8818e49d1b29
Claudio Brandolino <cbrandolino@gmail.com>
2012-09-19 06:08:15 +0200

    Video insertion is now dynamic

---
+++ README.md
@@ -0,0 +1,5 @@
+= Hai guise!
+
+Please don't even look at this stuff right now. I just needed to store the
+repo somewhere and I'm too lazy to setup bitbucket and too cheap to pay for
+a private repo.
--- js/base.js
+++ js/base.js
@@ -6,7 +6,11 @@ navigator.getUserMedia  = navigator.getUserMedia ||
                           navigator.msGetUserMedia
 
 window.onload = function() {
-  var video = document.querySelector('video');
+  var streamContainer = document.createElement('div')
+  var video = document.createElement('video')
+  video.setAttribute('autoplay', '1')
+  streamContainer.appendChild(video)
+  document.body.appendChild(streamContainer)
 
   navigator.getUserMedia({video: true}, function(stream) {
     video.src = window.URL.createObjectURL(stream)
